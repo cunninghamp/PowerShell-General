5ff352c93d77f9148b6adb1c13847ee2d1b461ae
Paul Cunningham <paul@locklan.com.au>
2018-10-22 07:57:21 +1000

    Update Get-PublicIPAddress.ps1

--- Get-PublicIPAddress/Get-PublicIPAddress.ps1
+++ Get-PublicIPAddress/Get-PublicIPAddress.ps1
@@ -27,9 +27,9 @@ Written by: Paul Cunningham
 
 Find me on:
 
-* My Blog:	http://paulcunningham.me
+* My Blog:	https://paulcunningham.me
 * Twitter:	https://twitter.com/paulcunningham
-* LinkedIn:	http://au.linkedin.com/in/cunninghamp/
+* LinkedIn:	https://au.linkedin.com/in/cunninghamp/
 * Github:	https://github.com/cunninghamp
 
 Change Log
